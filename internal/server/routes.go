f45334e4b02f8b7651a6903aa6862220883b1845
Devendra Mundada <devd251993@gmail.com>
2024-07-26 01:30:32 +0530

    added routes

---
+++ internal/server/routes.go
@@ -0,0 +1,30 @@
+package server
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"github.com/devd251993/applied_systems_assessment/internal/handlers"
+)
+
+func InitRoutes() *mux.Router {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/graph/create", handlers.CreateGraphHandler).Methods("POST")
+	router.HandleFunc("/graph/get", handlers.GetGraphHandler).Queries("id", "{id}").Methods("GET")
+	router.HandleFunc("/graph/shortest-path", handlers.GetShortestPathHandler).Queries("start", "{start}").Queries("end", "{end}").Methods("GET")
+	router.HandleFunc("/graph/delete", handlers.DeleteGraphHandler).Queries("id", "{id}").Methods("DELETE")
+
+	router.Use(Middleware)
+	return router
+}
+
+func Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.RequestURI)
+		w.Header().Set("Content-Type", "json")
+		next.ServeHTTP(w, r)
+	})
+}
